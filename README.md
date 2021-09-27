39d1b7d8be2d9981c72b8743f8ce4fd41bd9049c
Lienard Colin <colin.lienard@etu.unilim.fr>
2021-09-27 13:23:21 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Discord bot
+
+- [Discord.js documentation](https://discordjs.guide/)
+- [Discord Applications](https://discord.com/developers/applications/)
---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'server') {
+    await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+  } else if (commandName === 'user') {
+    await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+  }
+});
+
+client.login(process.env.TOKEN);
